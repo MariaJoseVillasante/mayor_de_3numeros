49f754f5c95c39b46219594c425bf8d439f89220
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-04 19:17:25 -0400

    version 1

---
+++ mayor_de_3.rb
@@ -0,0 +1,19 @@
+numero1 = ARGV[0];
+numero2 = ARGV[1];
+numero3 = ARGV[2];
+
+if numero1 >= numero2 
+    if numero1 >= numero2
+        puts numero1
+    elsif numero3 >= numero1
+        puts numero3
+    end
+elsif numero2 >= numero1
+    if numero2 >= numero3
+        puts numero2
+    else
+        puts numero3
+    end
+else
+    puts numero3
+end
