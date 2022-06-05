e456c4a84028b929db676a5a2c4cf092acd67488
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-05 09:35:46 -0400

    arreglo

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -1,6 +1,6 @@
-numero1 = ARGV[0];
-numero2 = ARGV[1];
-numero3 = ARGV[2];
+numero1 = ARGV[0].to_i;
+numero2 = ARGV[1].to_i;
+numero3 = ARGV[2].to_i;
 
 if numero1 >= numero2 
     if numero1 >= numero3
