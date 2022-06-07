2b34e3d4cd5d3bad1c14c4e6619597c8a27b2b19
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-07 11:43:21 -0400

    agregando .max

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -2,6 +2,8 @@ numero1 = ARGV[0].to_i;
 numero2 = ARGV[1].to_i;
 numero3 = ARGV[2].to_i;
 
+print [numero1,numero2,numero3].max
+
 if numero1 >= numero2 
     if numero1 >= numero3
         puts numero1
